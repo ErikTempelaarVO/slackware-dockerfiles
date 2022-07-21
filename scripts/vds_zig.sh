e900ca982749a522082725a7169d443a2f9e2d63
Erik Tempelaar <Erik.Tempelaar@vanoord.com>
2022-07-21 16:36:46 +0200

    Change order (multilib before template) and add vds_zig template

---
+++ scripts/vds_zig.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+cd /root && wget --no-check-certificate https://ziglang.org/builds/zig-linux-x86_64-0.10.0-dev.3027+0e26c6149.tar.xz && tar xvzf zig-linux-x86_64-0.10.0-dev.3027+0e26c6149.tar.xz
